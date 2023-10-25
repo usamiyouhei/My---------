35e01aa295f8e5d1c25bd268e2d9d9882ed19cca
youhei <usao9110@icloud.com>
2023-10-25 15:49:04 +0900

    変更２

--- コーディング練習/main.js
+++ コーディング練習/main.js
@@ -3,3 +3,33 @@ document.querySelector('.hbg-btn').addEventListener('.click', function(){
   this.classList.toggle('.active');
   document.querySelector('.menu').classList.toggle('.active');
 })
+
+//スワイパー
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+});
+
+//スムーススクロール
+//aタグの#取得
+const anchors = document.querySelectorAll('a[href^="#"]');
+//headerの高さ
+const headerHeight = document.querySelector('header').offsetHeight;
+//アンカー取得
+const urlHash = location.href;
+for(let i = 0; i < anchors.length; i++){
+  anchors[i].addEventListener('click', function(event){
+    event.preventDefault();
+    const href = anchors[i].getAttribute("href");
+
+    // スクロース先の要素取得
+    const target = document.getElementById(href.replace('#', ''));
+
+    // スクロール位置
+    const position = window.pageYOffset + target.getBoundingClientRect().top - headerHeight;
+    window.scroll({
+      top: position,
+      behavior: 'smooth'
+    });
+  });
