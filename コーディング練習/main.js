6f74e117fdaa52cb45b7697008f14302c541ea47
youhei <usao9110@icloud.com>
2023-10-30 11:20:10 +0900

    変更３

--- コーディング練習/main.js
+++ コーディング練習/main.js
@@ -1,7 +1,7 @@
 // .hbg-btn
 document.querySelector('.hbg-btn').addEventListener('.click', function(){
   this.classList.toggle('.active');
-  document.querySelector('.header-nav').classList.toggle('.active');
+  document.querySelector('.slide-menu').classList.toggle('.active');
 });
 
 //スワイパー
