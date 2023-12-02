66bb7425b9864f9b6ae5ea52ab04373b734b4821
youhei usami <usao9110@icloud.com>
2023-12-02 20:57:15 +0900

    my portfolio

--- myportfolio2/main.js
+++ myportfolio2/main.js
@@ -59,3 +59,17 @@ window.addEventListener('scroll', function(){
     }
   }
 });
+//scroll_effect
+$(window).scroll(function () {
+  var scrollAnimationElm = document.querySelectorAll('.scroll_up');
+  var scrollAnimationFunc = function () {
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 100;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
