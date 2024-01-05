0693c6a7dab81cfc141e84bbb2de99f7f4263773
youhei usami <usao9110@icloud.com>
2024-01-05 12:25:56 +0900

    my-portfolio

---
+++ website/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+  $(".js-hamburger,.js-drawer").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  });
+});
+// スライダー
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  effect: "fade",
+  speed: 3000,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 3000,
+  },
+});
